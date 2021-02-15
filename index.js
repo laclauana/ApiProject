5db7f4e018cb4a538da42ec124c98cb76e72fd8c
anaclette <laclauana@gmail.com>
2021-02-15 02:22:17 -0300

    done with responsive design

--- index.js
+++ index.js
@@ -1,31 +1,16 @@
-const link = document.createElement('button');
-const body = document.body;
 const urlBase = 'https://gateway.marvel.com/v1/public/';
 const apiKey = '5815682df904a6080be6caaebd915b02';
 const comicsPorPagina = 20;
 let paginaActual = 0;
 
-body.appendChild(link);
-link.setAttribute('id', 'prox');
-link.textContent = '>';
-
 const buscarInfo = (URL) => {
 	fetch(URL).then((res) => res.json()).then((data) => {
-		console.log(data);
-		const comics = data.data.results;
+		// console.log(data);
+		const link = document.querySelector('#link');
 		const seccion = document.querySelector('.resultados');
-		const link = document.querySelector('#prox');
-
-		link.onclick = (e) => {
-			e.preventDefault();
-			paginaActual++;
-			buscarInfo(
-				`${urlBase + 'comics?apikey=' + apiKey + '&orderBy=title&offset=' + paginaActual * comicsPorPagina}`
-			);
-		};
+		const comics = data.data.results;
 
 		seccion.innerHTML = '';
-
 		comics.map((comic) => {
 			seccion.innerHTML += `
                             <article>
@@ -33,11 +18,41 @@ const buscarInfo = (URL) => {
                             <img src="${comic.thumbnail.path}/portrait_uncanny.${comic.thumbnail.extension}">
                             </div>
                             <div>                                               
-                                    <p>${comic.title}</p>                               
+                            <p class="comic" data-id=${comic.id}>${comic.title}</p>                               
                             </div>
                         </article>
                         `;
 		});
+
+		link.onclick = () => {
+			paginaActual++;
+			buscarInfo(
+				`${urlBase + 'comics?apikey=' + apiKey + '&orderBy=title&offset=' + paginaActual * comicsPorPagina}`
+			);
+		};
+
+		const comicsHTML = document.querySelectorAll('.comic');
+		// console.log(comicsHTML);
+
+		// comicsHTML.forEach((comic) => {
+		// 	comic.onclick = () => {
+		// 		console.log(comic.dataset.id);
+		// 		// console.log('hiciste click a un comic');
+		// 		fetch(
+		// 			`${urlBase} + 'comics/' + ${comic.dataset
+		// 				.id} + '?apikey=' + ${apiKey} + '&orderBy=title&offset=' + ${paginaActual} * ${comicsPorPagina}`
+		// 		)
+		// 			.then((res) => res.json())
+		// 			.then((dataComic) => console.log(dataComic));
+
+		// seccion.innerHTML = '';
+		// seccion.innerHTML +=
+		// NUEVO ARTICLE CON EL DATACOMIC
+		// mismo procedimiento con dataComicPersonaje
+
+		// funciones reutilizables
+		// 	};
+		// });
 	});
 };
 
@@ -65,14 +80,38 @@ botonBuscar.onclick = (e) => {
 	e.preventDefault();
 };
 
+// const buscarComics = (url, paginaActual, nombre) => {
+// 	console.log('buscando comics...');
+// 	fetch(`${urlBase + url}?apikey=${apiKey}&offset=${paginaActual * comicsPorPagina}`)
+// 		.then((res) => res.json())
+// 		.then((data) => {
+// 			console.log(data);
+// 			personajes = data.data.results;
+// 			const seccion = document.querySelector('.resultados');
+// 			seccion.innerHTML = '';
+// 			personajes.map((personaje) => {
+// 				seccion.innerHTML += `<p>${personaje[nombre]}</p>`;
+// 			});
+// 		});
+// };
+
+// buscarComics('comics', paginaActual, 'title');
+// buscarComics('characters', paginaActual, 'name');
+
 const buscarPersonajes = (paginaActual) => {
-	fetch(`${paginaActual * comicsPorPagina}`).then((res) => res.json()).then((data) => {
-		console.log(data);
-		personajes = data.data.results;
-		const seccion = document.querySelector('.resultados');
-		seccion.innerHTML = '';
-		personajes.map((personaje) => {
-			seccion.innerHTML += `<p>${personaje.name}</p>`;
+	fetch(`${urlBase + 'characters?apikey=' + apiKey + '&offset=' + paginaActual}`)
+		.then((res) => res.json())
+		.then((data) => {
+			console.log(data);
+			personajes = data.data.results;
+			const seccion = document.querySelector('.resultados');
+			seccion.innerHTML = '';
+			personajes.map((personaje) => {
+				seccion.innerHTML += `<p>${personaje.name}</p>`;
+			});
 		});
-	});
 };
+// collectionURI en Preview pestania para tomar datos de characters y etc
+
+// estructura final de la URL a la que hacemos fetch:
+// url de la api + coleccion que buscamos + ? + queryParam=valor + & + queryParam=valor
