3ac8a7f90b664a96b7a9ac15a27cac0a7ad277ef
anaclette <laclauana@gmail.com>
2021-03-13 13:57:14 -0300

    switched coding language and worked on few functions

--- index.js
+++ index.js
@@ -1,145 +1,244 @@
-const urlBase = 'https://gateway.marvel.com/v1/public/';
+// --------------------------------- Variable declaration ----------------------------
+const baseURL = 'https://gateway.marvel.com/v1/public/';
 const apiKey = '5815682df904a6080be6caaebd915b02';
-const comicsPorPagina = 20;
-let paginaActual = 0;
-// const paginasAtras = document.querySelector('#saltear-atras');
-// const paginasAdelante = document.querySelector('#saltear-adelante');
-// const atras = document.querySelector('#atras');
-// console.log(paginasAdelante);
-// console.log(paginasAtras);
-// console.log(atras);
+const searchInput = document.querySelector('#search-input');
+const typeSelect = document.querySelector('#type');
+const orderSelect = document.querySelector('#order');
+const searchButton = document.querySelector('.search');
+// const firstPage = document.querySelector('#first-page');
+// const nextPage = document.querySelector('#next-page');
+// const previousPage = document.querySelector('#previous-page');
+// const lastPage = document.querySelector('#last-page');
+const resultsSection = document.querySelector('.results');
+const shownComics = document.querySelector('.title > p');
+const cardsPerPage = 20;
+let currentPage = 0;
 
-const buscarInfo = (URL) => {
-	fetch(URL).then((res) => res.json()).then((data) => {
-		// console.log(data);
-		const siguiente = document.querySelector('#siguiente');
-		const seccion = document.querySelector('.resultados');
-		const comics = data.data.results;
-
-		seccion.innerHTML = '';
-		comics.map((comic) => {
-			seccion.innerHTML += `
-                            <article>
-                            <div class="imagen">
-                            <img src="${comic.thumbnail.path}/portrait_uncanny.${comic.thumbnail.extension}">
-                            </div>
-                            <div>                                               
-                            <p class="comic" data-id=${comic.id}>${comic.title}</p>                               
-                            </div>
-                        </article>
-                        `;
-		});
-
-		const comicsMostrados = document.querySelector('.titulo > p');
-		comicsMostrados.innerHTML = '';
-		let cantidadComics = data.data.total;
-		comicsMostrados.textContent = `Mostrando ${cantidadComics} resultados`;
-		siguiente.onclick = () => {
-			paginaActual++;
-			buscarInfo(
-				`${urlBase + 'comics?apikey=' + apiKey + '&orderBy=title&offset=' + paginaActual * comicsPorPagina}`
-			);
-		};
-
-		const primeraPagina = document.querySelector('#primera-pagina');
-		const ultimaPagina = document.querySelector('#ultima-pagina');
-		const atras = document.querySelector('#atras');
-		ultimaPagina.onclick = () => {
-			paginaActual += 2;
-			console.log('paginas adelante', paginaActual);
-			buscarInfo(
-				`${urlBase + 'comics?apikey=' + apiKey + '&orderBy=title&offset=' + paginaActual * comicsPorPagina}`
-			);
-		};
-
-		atras.onclick = () => {
-			paginaActual--;
-			buscarInfo(
-				`${urlBase + 'comics?apikey=' + apiKey + 'orderBy=title&offset' + paginaActual * comicsPorPagina}`
-			);
-		};
-
-		primeraPagina.onclick = () => {
-			paginaActual = 0;
-		};
-
-		const comicsHTML = document.querySelectorAll('.comic');
-		// console.log(comicsHTML);
-
-		// comicsHTML.forEach((comic) => {
-		// 	comic.onclick = () => {
-		// 		console.log(comic.dataset.id);
-		// 		// console.log('hiciste click a un comic');
-		// 		fetch(
-		// 			`${urlBase} + 'comics/' + ${comic.dataset
-		// 				.id} + '?apikey=' + ${apiKey} + '&orderBy=title&offset=' + ${paginaActual} * ${comicsPorPagina}`
-		// 		)
-		// 			.then((res) => res.json())
-		// 			.then((dataComic) => console.log(dataComic));
-
-		// seccion.innerHTML = '';
-		// seccion.innerHTML +=
-		// NUEVO ARTICLE CON EL DATACOMIC
-		// mismo procedimiento con dataComicPersonaje
-	});
-};
-
-buscarInfo(`${urlBase + 'comics?apikey=' + apiKey + '&orderBy=title&offset=' + paginaActual * comicsPorPagina}`);
-
-const inputBusqueda = document.querySelector('#input-busqueda');
-inputBusqueda.oninput = () => {
-	console.log(inputBusqueda.value);
-};
-const selectTipo = document.querySelector('#tipo');
-const selectOrden = document.querySelector('#orden');
-const botonBuscar = document.querySelector('.buscar');
-
-selectTipo.onchange = () => {
-	const opcionTipo = selectTipo.options[selectTipo.selectedIndex].value;
-	console.log(opcionTipo);
-};
-
-selectOrden.onchange = () => {
-	const opcionOrden = selectOrden.options[selectOrden.selectedIndex].value;
-	console.log(opcionOrden);
-};
-
-botonBuscar.onclick = (e) => {
-	e.preventDefault();
-};
-
-// const buscarComics = (url, paginaActual, nombre) => {
-// 	console.log('buscando comics...');
-// 	fetch(`${urlBase + url}?apikey=${apiKey}&offset=${paginaActual * comicsPorPagina}`)
-// 		.then((res) => res.json())
-// 		.then((data) => {
-// 			console.log(data);
-// 			personajes = data.data.results;
-// 			const seccion = document.querySelector('.resultados');
-// 			seccion.innerHTML = '';
-// 			personajes.map((personaje) => {
-// 				seccion.innerHTML += `<p>${personaje[nombre]}</p>`;
-// 			});
-// 		});
-// };
-
-// buscarComics('comics', paginaActual, 'title');
-// buscarComics('characters', paginaActual, 'name');
-
-const buscarPersonajes = (paginaActual) => {
-	fetch(`${urlBase + 'characters?apikey=' + apiKey + '&offset=' + paginaActual}`)
+const fetchComics = (currentPage, cardsPerPage, collection = 'comics') => {
+	fetch(`${baseURL}${collection}?apikey=${apiKey}&orderBy=title&offset=${currentPage * cardsPerPage}`)
 		.then((res) => res.json())
 		.then((data) => {
-			console.log(data);
-			personajes = data.data.results;
-			const seccion = document.querySelector('.resultados');
-			seccion.innerHTML = '';
-			personajes.map((personaje) => {
-				seccion.innerHTML += `<p>${personaje.name}</p>`;
+			const comics = data.data.results;
+			resultsSection.innerHTML = '';
+			comics.map((comic) => {
+				resultsSection.innerHTML += `
+                            <article class="comic" data-id=${comic.id}>
+                            <div class="img-container">
+                            <img src="${comic.thumbnail.path}/portrait_uncanny.${comic.thumbnail
+					.extension}" alt="${comic.title}">
+                            </div>
+                            <div>                                               
+                            <p>${comic.title}</p>                               
+                            </div>
+                        </article> 	
+                        `;
+
+				const shownComics = document.querySelector('.title > p');
+				shownComics.innerHTML = '';
+				let comicsQuantity = data.data.total;
+				shownComics.textContent = `Mostrando ${comicsQuantity} resultados`;
+
+				const comicsHTML = document.querySelectorAll('.comic');
+				comicsHTML.forEach((comic) => {
+					comic.onclick = () => {
+						// console.log(comic);
+						// console.log('hiciste click a un comic');
+						console.log(comic.dataset.id);
+						htmlCards();
+					};
+				});
+				updatePagination(collection);
+				// console.log(data.data.offset);
 			});
 		});
 };
-// collectionURI en Preview pestania para tomar datos de characters y etc
 
-// estructura final de la URL a la que hacemos fetch:
-// url de la api + coleccion que buscamos + ? + queryParam=valor + & + queryParam=valor
+fetchComics(`${baseURL + 'comics?apikey=' + apiKey + '&orderBy=title&offset=' + currentPage * cardsPerPage}`);
+
+const htmlCards = (collection = 'characters') => {
+	fetch(`${baseURL}${collection}?apikey=${apiKey}`).then((res) => res.json()).then((json) => {
+		const characters = json.data.results;
+		// console.log(characters);
+		characters.forEach((character) => {
+			resultsSection.innerHTML = '';
+			const date = new Date(character.modified);
+			return (resultsSection.innerHTML += `
+					<article class="comic" data-id=${character.id}>
+						<div class="img-container">
+						<img src="${noAvailableImg(character)}" alt="imagen de ${character.name}"/>
+						</div>;
+							<div>
+							<p>${character.name}</p>
+							</div>
+							<p>Publicado: ${date.toLocaleDateString() === 'Invalid Date' ? 'No disponible' : date.toLocaleDateString()}</p>
+							<p>Descripcion: ${character.description || 'No disponible'} </p>
+							</article>
+									
+					<article class="comic"
+					  <div class="img-container">
+						<img src="${noAvailableImg(character)}" alt="imagen de ${character.name}"/>
+					  </div>
+					  <div class="img-container">
+						<p>${character.name}</p>
+					  </div>
+					</article>
+			`);
+		});
+	});
+};
+
+const updateResultsQuantity = (collection) => {
+	// fetch(`${baseURL + collection + '?apikey=' + apiKey}`).then((res) => res.json()).then((data) => {
+	// 	const shownComics = document.querySelector('.title > p');
+	// 	shownComics.innerHTML = '';
+	// 	let comicsQuantity = data.data.total;
+	// 	return comicsQuantity != 0
+	// 		? `Mostrando ${comicsQuantity} resultados`
+	// 		: (shownComics.textContent = 'No están estos resultados 😪');
+	// });
+};
+
+const updatePagination = (collection = 'comics') => {
+	const firstPage = document.querySelector('#first-page');
+	const nextPage = document.querySelector('#next-page');
+	const previousPage = document.querySelector('#previous-page');
+	const lastPage = document.querySelector('#last-page');
+	// let offset = currentPage * cardsPerPage;
+
+	// offset == 0
+	// 	? ((previousPage.disabled = true), (firstPage.disabled = true))
+	// 	: ((previousPage.disabled = false), (firstPage.disabled = false));
+
+	// offset + cardsPerPage >= comicsQuantity
+	// 	? ((nextPage.disabled = true), (lastPage.disabled = true))
+	// 	: ((nextPage.disabled = false), (lastPage.disabled = false));
+
+	nextPage.onclick = () => {
+		currentPage++;
+		console.log(currentPage);
+	};
+
+	lastPage.onclick = () => {
+		const remainder = data.data.total % cardsPerPage;
+		if (remainder > 0) {
+			currentPage = (data.data.total - remainder) / cardsPerPage;
+		} else {
+			currentPage = (data.data.total - remainder) / cardsPerPage - cardsPerPage;
+		}
+		nextPage.disabled = true;
+		lastPage.disabled = true;
+		console.log(currentPage);
+	};
+
+	previousPage.onclick = () => {
+		currentPage--;
+		console.log(currentPage);
+		resultsSection.innerHTML = '';
+		fetchComics(
+			`${baseURL + collection + '?apikey=' + apiKey + '&orderBy=title&offset=' + currentPage * cardsPerPage}`
+		);
+	};
+
+	firstPage.onclick = () => {
+		currentPage = 0;
+		firstPage.disabled = true;
+		previousPage.disabled = true;
+	};
+};
+
+searchInput.value = '';
+searchInput.oninput = () => {
+	console.log(searchInput.value);
+	// search();
+};
+
+typeSelect.onchange = () => {
+	const typeOption = typeSelect.options[typeSelect.selectedIndex].value;
+	console.log(typeOption);
+	// search();
+};
+
+orderSelect.onchange = () => {
+	const orderOption = orderSelect.options[orderSelect.selectedIndex].value;
+	console.log(orderOption);
+	// search();
+};
+
+// const search = () => {
+// 	currentPage = 0;
+// 	if (typeOption === 'characters') {
+// 		fetchCharacters('characters');
+// 	}
+// 	if (typeOption === 'comics') {
+// 		fetchComics(currentPage, cardsPerPage, (collection = 'comics'));
+// 	}
+// 	updateResultsQuantity(collection);
+// };
+
+searchButton.onclick = (e) => {
+	e.preventDefault();
+	// search();
+};
+
+const noAvailableImg = (data) => {
+	return data.thumbnail.path.includes('image_not_available')
+		? 'assets/img no disponible.jpg'
+		: `${data.thumbnail.path}.${data.thumbnail.extension}`;
+};
+
+// 	info.data.results.map((comic) => {
+// 		const writer = creatorsListHasWriter(comic);
+
+// 		const characters = comic.characters.available
+// 			? displayIncludedCharacters(comic, comic.id)
+// 			: updateAvailableCharacters(comic);
+
+// 		return (resultsSection.innerHTML = `
+//     <article class="comic" id="${comic.id}">
+//       <div class="img-container">
+//         <img src="${imgURL}" alt="imagen de ${comic.title}"/>
+//       </div>
+//       <div>
+//       <p>
+//         ${comic.title}
+//       </p>
+//       <p>Publicado:</p>
+//       <p>${onSaleDate}</p>
+//       <p>Guionista/s:<p>
+//       <p>${writer}</p>
+//       <p>Descripción:</p>
+//       <p>${comic.description || `Descripción no disponible`}</p>
+//       </div>
+//     </article>`);
+// 	});
+// };
+
+// const updateAvailableCharacters = (comic) => {
+// 	totalItems = comic.characters.available;
+// 	shownComics.textContent = `Mostrando ${totalItems} resultados`;
+// };
+
+// const getCharacterInfo = (info) => {
+// 	clearSectionContent(charactersSection);
+// 	info.data.results.map((character) => {
+// 		const imgURL = noAvailableImg(character);
+// 		shownComics.textContent = `Comics`;
+// 		const comics = character.comics.available
+// 			? displayComicsContainingCharacter(character, character.id)
+// 			: updateAvailableComics(character);
+
+// 		return (charactersSection.innerHTML = `
+//     <article class="comic" id="${character.id}">
+//       <div class="img-container">
+//         <img src="${imgURL}" alt="imagen de ${character.name}"/>
+//       </div>
+//       <div>
+//       <p>
+//         ${character.name}
+//       </p>
+//       <p>Descripción:</p>
+//       <p>${character.description || noInfoMsg}</p>
+//       </div>
+//     </article>`);
+// 	});
+// };
